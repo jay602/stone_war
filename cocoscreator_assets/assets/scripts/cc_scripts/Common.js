32023c58173c2eb25fa07ebada83b1a30e3bb4f7
jay602 <857024709@qq.com>
2018-04-18 17:10:47 +0800

    修改走动的一些小异常，随机生成用户名，fix jumpanim not found

---
+++ cocoscreator_assets/assets/scripts/cc_scripts/Common.js
@@ -0,0 +1,40 @@
+window.Action = {
+    Stop : 0,
+    MoveLeft: 1,
+    MoveRight: 2,
+    StopMoveLeft: 3,
+    StopMoveRight: 4,
+    JumpUp: 5,
+    JumpLeft: 6,
+    JumpRight: 7,
+    MoveJumpLeft: 8,
+    MoveJumpRight: 9,
+    AdjustThrow: 10,
+    Throw: 11,
+    Die: 12,
+};
+
+window.State = {
+    STOP : 0,
+    MOVE_LEFT : 1,
+    MOVE_RIGHT : 3,
+    JUMP : 4,
+    JUMP_LEFT : 5,
+    JUMP_RIGHT : 6,
+    MOVE_LEFT_JUMP: 7,
+    MOVE_RIGHT_JUMP: 8,
+    DIE : 7,
+};
+
+window.STATIC = 0;
+window.MOVE_LEFT = 1;
+window.MOVE_RIGHT = 2;
+window.MOVE_UP = 3;
+window.MOVE_DOWN = 4;
+
+window.SCALE = 10;
+
+
+window.GlobalY = 0;
+
+
